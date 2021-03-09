281f372baa4d846b34fefa89d1a22031a73d1fb4
lwqlwq60 <liuweiqian01@163.com>
2021-03-09 17:07:27 +0800

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,4 +2,4 @@
 
 set -o errexit
 
-sudo apt update && sudo apt install -y git ansible && sudo ansible-pull -i hosts -U https://github.com/jinweijie/ansible-provision
+sudo apt update && sudo apt install -y git ansible && sudo ansible-pull -i hosts -U https://github.com/lwqlwq60/ansible-provision
